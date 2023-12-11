34b396d83925dd421df3b491939dcaff0b1aa092
StephenRicketts <stephen.ricketts12@gmail.com>
2023-12-11 15:22:21 -0400

    added contact page and notification

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,18 @@
 import FeaturedPosts from "@/components/home-page/featured-posts";
 import Hero from "@/components/home-page/hero";
 import { getFeaturedPosts } from "@/lib/posts-util";
-
-// const DUMMY_POSTS = [
-//   {
-//     slug: "getting-started-with-nextjs1",
-//     title: "NextJS1",
-//     image: "getting-started-nextjs.png",
-//     except: "NextKS is the React framework for production.",
-//     date: "2023-02-10",
-//   },
-//   {
-//     slug: "getting-started-with-nextjs2",
-//     title: "NextJS2",
-//     image: "getting-started-nextjs.png",
-//     except: "NextKS is the React framework for production.",
-//     date: "2023-02-10",
-//   },
-//   {
-//     slug: "getting-started-with-nextjs3",
-//     title: "NextJS3",
-//     image: "getting-started-nextjs.png",
-//     except: "NextKS is the React framework for production.",
-//     date: "2023-02-10",
-//   },
-// ];
+import Head from "next/head";
 
 function HomePage(props) {
   return (
     <>
+      <Head>
+        <title>Welcome to my blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web development."
+        />
+      </Head>
       <Hero />
       <FeaturedPosts posts={props.posts} />
     </>
